c0fe00ee32f7cefcc2858fb34429a2fb3d0a5afe
abdelouafi <abdelouafioubenali.com>
2025-04-17 00:21:46 +0100

    Résoudre un problème de nom de fichier.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Menu, X } from 'lucide-react';
-import Main from './Main';
+import Main from './test';
 import NavLink from './components/NavLink';
 import MobileNavLink from './components/MobileNavLink';
 
